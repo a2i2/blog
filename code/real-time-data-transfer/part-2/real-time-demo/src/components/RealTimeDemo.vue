392ab216013b31e0aa523db90e0bb078c3e87759
r-hyndman <37819011+r-hyndman@users.noreply.github.com>
2020-12-10 12:05:19 +1100

    ENHANCE: Change column width of chat component

--- code/real-time-data-transfer/part-2/real-time-demo/src/components/RealTimeDemo.vue
+++ code/real-time-data-transfer/part-2/real-time-demo/src/components/RealTimeDemo.vue
@@ -1,8 +1,10 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12">
+      <v-col cols="4">
         <chat />
+      </v-col>
+      <v-col cols="8">
       </v-col>
     </v-row>
   </v-container>
